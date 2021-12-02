c5d47311a6b5359890f26e7230c37c305c0c690c
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2021-12-02 14:01:42 +0200

    day 2 solution

--- src/aatkin/aoc_21.clj
+++
@@ -1,7 +0,0 @@
-(ns aatkin.aoc-21
-  (:gen-class)
-  (:require [aatkin.day-1 :as day-1]))
-
-(comment
-  (day-1/part-1-solution)
-  (day-1/part-2-solution))
---
+++ src/aatkin/day_2.clj
@@ -0,0 +1,62 @@
+(ns aatkin.day-2
+  (:require [clojure.string :as s]))
+
+(def ^:private input (slurp "resources/input/day_2_input"))
+(def ^:private mock-input (slurp "resources/mock_input/day_2_mock_input"))
+
+(defn- parse [input]
+  (->> (s/split-lines input)
+       (map #(s/split % #" "))
+       (map (fn [[type value]] [type (Integer/parseInt value)]))))
+
+(defn horizontal-pos? [[type]]
+  (= "forward" type))
+(defn depth-value [[type n]]
+  (if (= "up" type) (- n) n))
+
+(defn get-final-position [input]
+  (let [horizontals (map second (filter horizontal-pos? input))
+        depths (map depth-value (remove horizontal-pos? input))]
+    (* (apply + horizontals)
+       (apply + depths))))
+
+(defn part-1-solution []
+  (get-final-position (parse input)))
+
+(comment
+  (parse mock-input)
+  (get-final-position (parse mock-input))
+  (get-final-position (parse input))
+  )
+
+(defn calculate-aim [[type n]]
+  (condp = type
+    "down" n
+    "up" (- n)
+    0))
+
+(defn calculate-horizontal [[type n]]
+  (if (= type "forward")
+    n 0))
+
+(defn calculate-depth [[type n] aim]
+  (if (= type "forward")
+     (* n aim) 0))
+
+(defn get-final-position-corrected [values]
+  (loop [values values
+         aim 0
+         horizontal 0
+         depth 0]
+    (if-let [current (first values)]
+      (recur (rest values)
+             (+ aim (calculate-aim current))
+             (+ horizontal (calculate-horizontal current))
+             (+ depth (calculate-depth current aim)))
+      (* horizontal depth))))
+
+(comment
+  (parse mock-input)
+  (get-final-position-corrected (parse mock-input))
+  (get-final-position-corrected (parse input))
+  )
