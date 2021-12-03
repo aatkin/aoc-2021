9a4215f62965baadb013a0acd67528f0b4c90f30
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2021-12-03 13:41:17 +0200

    refactor day 3

--- src/aatkin/day_3.clj
+++ src/aatkin/day_3.clj
@@ -4,84 +4,71 @@
 (def ^:private input (slurp "resources/input/day_3"))
 (def ^:private mock-input (slurp "resources/mock_input/day_3"))
 
-(defn- parse [input]
-  (->> (s/split-lines input)))
+(defn- parse [input] (s/split-lines input))
 
-(defn- get-nth-bits [data n]
-  (map #(nth % n) data))
+(defn- transpose [coll] (apply map vector coll))
 
-(defn- find-bit-average [coll]
-  (/ (apply + (map #(Character/digit % 10) coll))
-     (count coll)))
+(defn- bin-to-int [str]
+  (Integer/parseInt str 2))
+(defn- char-to-int [c] (Character/digit c 10))
+(defn- chars-to-ints [coll] (map char-to-int coll))
+
+(defn- find-bit-averages [coll]
+  (let [bit-sums (->> (map chars-to-ints (transpose coll))
+                      (map #(apply + %)))
+        avg #(/ % (count coll))]
+    (map avg bit-sums)))
 
 (defn- most-common-bit [avg]
   (if (<= 0.5 avg) 1 0))
-
 (defn- least-common-bit [avg]
   (if (<= 0.5 avg) 0 1))
 
 (defn- gamma-rate [coll]
-  (->> (range (count (first coll)))
-       (map #(get-nth-bits coll %))
-       (map find-bit-average)
+  (->> (find-bit-averages coll)
        (map most-common-bit)))
 
 (defn- epsilon-rate [coll]
-  (->> (range (count (first coll)))
-       (map #(get-nth-bits coll %))
-       (map find-bit-average)
+  (->> (find-bit-averages coll)
        (map least-common-bit)))
-
-(defn- to-integer [coll]
-  (Integer/parseInt (s/join coll) 2))
 
 (defn part-1-solution []
   (->> ((juxt gamma-rate epsilon-rate) (parse input))
-       (map to-integer)
+       (map (comp bin-to-int s/join))
        (apply *)))
 
 (comment
-  (def coll (parse mock-input))
-  (gamma-rate coll)
-
   (->> ((juxt gamma-rate epsilon-rate) (parse mock-input))
-       (map to-integer)
+       (map (comp bin-to-int s/join))
        (apply *))
   
   (part-1-solution)
   )
 
 (defn- by-val-and-pos [coll pos n]
-  (filter #(= n (Character/digit (nth % pos) 10)) coll))
+  (filter #(= n (char-to-int (nth % pos))) coll))
 
-(defn- oxygen-generator-rating [coll]
+(defn- find-value-by [f coll]
   (let [bits (count (first coll))]
     (loop [coll coll
            pos 0]
       (if (or (> pos bits)
               (> (count coll) 1))
-        (recur (by-val-and-pos coll pos (nth (gamma-rate coll) pos))
+        (recur (by-val-and-pos coll pos (nth (f coll) pos))
                (inc pos))
         (first coll)))))
+(def ^:private oxygen-generator-rating (partial find-value-by gamma-rate))
+(def ^:private co2-scrubber-rating (partial find-value-by epsilon-rate))
 
-(defn- co2-scrubber-rating [coll]
-  (let [bits (count (first coll))]
-    (loop [coll coll
-           pos 0]
-      (if (or (> pos bits)
-              (> (count coll) 1))
-        (recur (by-val-and-pos coll pos (nth (epsilon-rate coll) pos))
-               (inc pos))
-        (first coll)))))
+(defn part-2-solution []
+  (->> ((juxt oxygen-generator-rating co2-scrubber-rating) (parse input))
+       (map (comp bin-to-int s/join))
+       (apply *)))
 
 (comment
-  (def coll (parse mock-input))
+  (->> ((juxt oxygen-generator-rating co2-scrubber-rating) (parse mock-input))
+       (map (comp bin-to-int s/join))
+       (apply *))
 
-  coll
-  (gamma-rate coll)
-  (by-val-and-pos coll 0 1)
-
-  ()
-
-  (oxygen-generator-rating coll)
-  (co2-scrubber-rating coll))
+  (part-2-solution)
+  )
