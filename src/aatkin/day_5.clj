131e5fbcc11b1d435e77e426ef3f21905be3ad13
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2021-12-05 15:56:01 +0200

    day 5 solutions part 1 and 2

---
+++ src/aatkin/day_5.clj
@@ -0,0 +1,73 @@
+(ns aatkin.day-5
+  (:require [clojure.string :as s]))
+
+(def ^:private input (slurp "resources/input/day_5"))
+(def ^:private mock-input (slurp "resources/mock_input/day_5"))
+
+(defn- parse-coords [s]
+  (let [[x y] (s/split s #",")]
+    [(Integer/parseInt x 10) (Integer/parseInt y 10)]))
+
+(defn- parse-part-1 [input]
+  (->> (s/split-lines input)
+       (map #(s/split % #" -> "))
+       (map (fn [[from to]]
+              [(parse-coords from) (parse-coords to)]))
+       (filter (fn [[from to]]
+                 (or (= (first from) (first to))
+                     (= (second from) (second to)))))))
+
+(defn- parse-part-2 [input]
+  (->> (s/split-lines input)
+       (map #(s/split % #" -> "))
+       (map (fn [[from to]]
+              [(parse-coords from) (parse-coords to)]))))
+
+(defn- plot-range [from to]
+  (if (< to from)
+    (range from (dec to) -1)
+    (range from (inc to))))
+
+(defn- plot-range-diagonal [[from-x to-x] [from-y to-y]]
+  (->> (interleave (plot-range from-x to-x)
+                   (plot-range from-y to-y))
+       (partition 2)))
+
+(defn- plot-path [[[from-x from-y] [to-x to-y]]]
+  (if (= from-x to-x) ;; x1 = x2
+    (map #(list from-x %) (plot-range from-y to-y))
+    (if (= from-y to-y) ;; y1 = y2
+      (map #(list % from-y) (plot-range from-x to-x))
+      (map #(list (first %) (second %)) ;; diagonal
+           (plot-range-diagonal [from-x to-x] [from-y to-y])))))
+
+(defn- plot-paths [data]
+  (loop [data data
+         paths {}]
+    (if (seq data)
+      (recur (rest data)
+             (reduce (fn [coll path]
+                       (assoc coll path (inc (get coll path 0))))
+                     paths
+                     (plot-path (first data))))
+      paths)))
+
+(defn part-1-solution []
+  (->> (parse-part-1 input) ;; ignore diagonal paths
+       plot-paths
+       (filter #(> (second %) 1))
+       count))
+
+(defn part-2-solution []
+  (->> (parse-part-2 input) ;; include diagonal paths
+       plot-paths
+       (filter #(> (second %) 1))
+       count))
+
+(comment
+  (plot-range-diagonal [1 1] [3 3])
+
+  (part-1-solution)
+
+  (part-2-solution)
+  )
